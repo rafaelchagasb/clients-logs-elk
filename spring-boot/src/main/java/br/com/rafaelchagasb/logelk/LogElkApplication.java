1f5714ad30ed5695a1ef939decbcf3e08378be33
Rafael Chagas <rafa15chagas@gmail.com>
2020-06-02 17:03:54 -0300

    integration spring-boot with ELK

---
+++ spring-boot/src/main/java/br/com/rafaelchagasb/logelk/LogElkApplication.java
@@ -0,0 +1,13 @@
+package br.com.rafaelchagasb.logelk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogElkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogElkApplication.class, args);
+	}
+
+}
---
+++ spring-boot/src/test/java/br/com/rafaelchagasb/logelk/LogElkApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rafaelchagasb.logelk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogElkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
